4b7166c4948a3e3dc895b99246ad1538d1a636db
seoyeon333 <sherlyn3663@gmail.com>
2023-04-21 18:17:17 +0900

    20230421
    repository 잘못 삭제하여 재생성

---
+++ 20230419/(1)_8393.py
@@ -0,0 +1,14 @@
+# BAEKJOON : 합 (8393번)
+
+# 문제 : n이 주어졌을 때, 1부터 n까지 합을 구하는 프로그램을 작성하시오.
+
+# 입력 : 첫째 줄에 n (1 ≤ n ≤ 10,000)이 주어진다.
+
+# 출력 : 1부터 n까지 합을 출력한다.
+
+n = int(input())
+answer = 0
+
+for i in range(1, n+1):
+    answer += i
+print(answer)
